57fe7ceda4ae64eaa443871d687106a84f3d3de2
Willy Aguirre <marti1125@gmail.com>
2013-10-05 16:22:32 -0500

    add jquery

--- js/app.js
+++ js/app.js
@@ -1,2 +1,10 @@
+$(document).on('ready',function(){
 
+	var map = L.map('map').setView([51.505, -0.09], 13);
+
+	L.tileLayer('http://{s}.tile.cloudmade.com/a8fd3134fc184ea79fb57dd7630c1101/997/256/{z}/{x}/{y}.png', {    
+	    maxZoom: 18
+	}).addTo(map);
+
+});
 
