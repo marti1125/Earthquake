a0e66b034045175bdfa34235c1659e458e8d2e77
Willy Aguirre <marti1125@gmail.com>
2013-10-16 08:57:52 -0500

    correct geojson

--- js/app.js
+++ js/app.js
@@ -3,17 +3,18 @@ $(document).on('ready',function(){
 	function popUp(feature, layer) {
 		layer.bindPopup(feature.properties.mag);
 	}	
+
+	var geojsonLayer = new L.GeoJSON.AJAX("http://www.corsproxy.com/earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",{onEachFeature:popUp});
+
 	
 	var layerMap = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png', {
 		key: "6798e89110614f5a9da04c5cd2918bf1",    
 	    maxZoom: 20
 	});
-
-	var geojsonLayer = new L.GeoJSON.AJAX("http://www.corsproxy.com/earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",{onEachFeature:popUp});
-
+	
 	var map = L.map('map').setView([51.505, -0.09], 13);
 
-	layerMap.addTo(map);
-  	geojsonLayer.addTo(map);
+	geojsonLayer.addTo(map);
+	layerMap.addTo(map);  	
 
 });
