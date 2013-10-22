8cc369b183d836dfc83e70195cbc0b4b22339cd4
Willy Aguirre <marti1125@gmail.com>
2013-10-22 01:03:12 -0500

    add style

--- js/app.js
+++ js/app.js
@@ -6,7 +6,6 @@ Zepto(function($){
 
 	var geojsonLayer = new L.GeoJSON.AJAX("http://www.corsproxy.com/earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",{onEachFeature:popUp});
 
-	
 	var layerMap = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png', {
 		key: "6798e89110614f5a9da04c5cd2918bf1",    
 	    maxZoom: 20
