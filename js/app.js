0e9a574ddcc55364e166d93db58aba4b99710a92
Willy Aguirre <marti1125@gmail.com>
2013-10-30 00:31:05 -0500

    correct l10n

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 Zepto(function($){
 
+    function geoData(data){
+        var geojsonLayer = new L.GeoJSON.AJAX("http://www.corsproxy.com/earthquake.usgs.gov/earthquakes/feed/v1.0/summary/"+data+".geojson",{onEachFeature:popUp}).addTo(map);
+        return geojsonLayer;
+    }
+
     var map = L.map('map').setView([51.505, -0.09], 13);
 
     function popUp(f,l){
@@ -12,14 +17,22 @@ Zepto(function($){
         }
     }
 
-	var geojsonLayer = new L.GeoJSON.AJAX("http://www.corsproxy.com/earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",{onEachFeature:popUp}).addTo(map);
+    var geojsonLayer = geoData("all_hour");
 
-	var layerMap = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png', {
-		key: "6798e89110614f5a9da04c5cd2918bf1",    
-	    maxZoom: 20
-	});
+    var layerMap = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png', {
+        key: "6798e89110614f5a9da04c5cd2918bf1",
+        maxZoom: 20
+    });
 
-	geojsonLayer.addTo(map);
-	layerMap.addTo(map);  	
+    geojsonLayer.addTo(map);
+    layerMap.addTo(map);
+
+    $("#btn-allEart-PastHour").on('click', function(){
+        geoData("all_hour");
+    });
+
+    $("#btn-M45-PastHour").on('click', function(){
+        geoData("4.5_week");
+    });
 
 });
