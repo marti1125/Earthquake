4e904e63a063a7504c1154e292726b4e9442553b
Willy Aguirre <marti1125@gmail.com>
2013-10-29 22:20:40 -0500

    show info about earthquake

--- js/app.js
+++ js/app.js
@@ -1,17 +1,23 @@
 Zepto(function($){
-	
-	function popUp(features, layer) {
-		layer.bindPopup(features.properties.mag);
-	}	
 
-	var geojsonLayer = new L.GeoJSON.AJAX("http://www.corsproxy.com/earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",{onEachFeature:popUp});
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+    function popUp(f,l){
+        var out = [];
+        if (f.properties){
+            for(key in f.properties){
+                out.push(key+": "+f.properties[key]);
+            }
+            l.bindPopup(out.join("<br />"));
+        }
+    }
+
+	var geojsonLayer = new L.GeoJSON.AJAX("http://www.corsproxy.com/earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson",{onEachFeature:popUp}).addTo(map);
 
 	var layerMap = L.tileLayer('http://{s}.tile.cloudmade.com/{key}/997/256/{z}/{x}/{y}.png', {
 		key: "6798e89110614f5a9da04c5cd2918bf1",    
 	    maxZoom: 20
 	});
-	
-	var map = L.map('map').setView([51.505, -0.09], 13);
 
 	geojsonLayer.addTo(map);
 	layerMap.addTo(map);  	
