8c9d47162495632f4676a56bf3ce366295848fad
Willy Aguirre <marti1125@gmail.com>
2013-11-01 12:35:57 -0500

    update all geojsons in maps

--- js/app.js
+++ js/app.js
@@ -43,6 +43,7 @@ Zepto(function($){
 
 	init();
 
+    //Past Hour
     $("#btn-sigEart-PastHour").on('click', function(){
         map.remove();
         map = L.map('map').setView([51.505, -0.09], 13);
@@ -59,7 +60,6 @@ Zepto(function($){
         map.remove();
         map = L.map('map').setView([51.505, -0.09], 13);
         update("4.5_hour");
-        geoData("2.5_hour");
     });
 
     $("#btn-M10-PastHour").on('click', function(){
@@ -72,6 +72,99 @@ Zepto(function($){
         map.remove();
         map = L.map('map').setView([51.505, -0.09], 13);
         update("all_hour");
+    });
+
+    //Past Day
+    $("#btn-sigEart-PastDay").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("significant_day");
+    });
+
+    $("#btn-M45-PastDay").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("4.5_day");
+    });
+
+    $("#btn-M25-PastDay").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("4.5_day");
+    });
+
+    $("#btn-M10-PastDay").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("1.0_day");
+    });
+
+    $("#btn-allEart-PastDay").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("all_day");
+    });
+
+    //Past 7 Days
+    $("#btn-sigEart-Past7Days").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("significant_week");
+    });
+
+    $("#btn-M45-Past7Days").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("4.5_week");
+    });
+
+    $("#btn-M25-Past7Days").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("4.5_week");
+    });
+
+    $("#btn-M10-Past7Days").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("1.0_week");
+    });
+
+    $("#btn-allEart-Past7Days").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("all_week");
+    });
+
+    //Past 30 Days
+    $("#btn-sigEart-Past30Days").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("significant_month");
+    });
+
+    $("#btn-M45-Past30Days").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("4.5_month");
+    });
+
+    $("#btn-M25-Past30Days").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("4.5_month");
+    });
+
+    $("#btn-M10-Past30Days").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("1.0_month");
+    });
+
+    $("#btn-allEart-Past30Days").on('click', function(){
+        map.remove();
+        map = L.map('map').setView([51.505, -0.09], 13);
+        update("all_month");
     });
 
 });
